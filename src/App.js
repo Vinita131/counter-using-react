edaf03ef70ba6354964bd34fd093182bb6fd77bb
unknown <you@example.com>
2021-09-01 02:10:08 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from "react";
+import Button from "./components/Button";
+import "./assets/css/style.css";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 1
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className="app">
+        
+       <div class="container">  
+
+            <div class="box">  
+
+                <div class="box-row"> 
+         
+          
+          <div class="buttons box-cell">
+            <Button title={"-"} action={this.decrementCount} />
+         </div>
+            <div class="count box-cell box2">
+            <h3>Count:</h3>
+            <h1>{count}</h1>
+          </div>
+           <div class="buttons box-cell ">
+            <Button title={"+"} action={this.incrementCount} />
+          </div>
+      </div>
+     </div>
     </div>
-  );
+    </div>
+    );
+  }
 }
 
-export default App;
